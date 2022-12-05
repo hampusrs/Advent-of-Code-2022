36b10967c943f717405e55218419f7001fd3d19f
rhedinh <rhedinh@chalmers.se>
2022-12-06 00:01:08 +0100

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,6 +1,6 @@
 def read_file(file):
     file = open(file, 'r')
-    data = None
+    data = []
 
     return data
 
@@ -9,8 +9,8 @@ def main():
     file =  "./input.txt"
     data = read_file(file)
 
-    ans1 = 0
-    print("Answer: %s" % ans1)
+    ans = 0
+    print("Answer: %s" % ans)
 
 if __name__ == '__main__':
     main()
